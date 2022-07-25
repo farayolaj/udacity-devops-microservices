ab0d79733dfab2c0dda45688abe33d875ae9eedf
Joshua Farayola <farayolajoshua@gmail.com>
2022-07-25 16:49:01 +0000

    Update script to run docker

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build -t housing-prediction:current .
 
 # Step 2: 
 # List docker images
+docker ps
 
 # Step 3: 
 # Run flask app
+docker run -p 8000:80 housing-prediction:current
