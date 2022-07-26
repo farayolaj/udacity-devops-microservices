05788ab1e466f5f202c07d4a16e7d02cbec6cf6e
Joshua Farayola <farayolajoshua@gmail.com>
2022-07-26 14:03:07 +0000

    Complete script to run kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
 
+# Use to recognize kubectl alias when using minikube
+shopt -s expand_aliases
+source ~/.bashrc
+
 # This tags and uploads an image to Docker Hub
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=farayolaj/housing-prediction
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment housing-pred --image=$dockerpath --port 80
 
 # Step 3:
 # List kubernetes pods
+sleep 5
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/housing-pred 8000:80
